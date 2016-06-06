8fe98646351ea12fa36da45162e4cdee0c738cb2
Hanna <86djachenko@gmail.com>
2016-06-06 20:38:12 +0300

    Creating calculator with wpf

---
+++ MethodsLibrary/MathMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MethodsLibrary
+{
+    public static class MathMethods
+    {
+        private static double Summ(double a, double b)
+        {
+            return (a + b);
+        }
+
+        private static double Diff(double a, double b)
+        {
+            return (a - b);
+        }
+
+        private static double Multiplication(double a, double b)
+        {
+            return (a * b);
+        }
+
+        private static double Division(double a, double b)
+        {
+            return (a * 1.0 / b);
+        }
+
+        public static double Calculate(double a, double b, string sign)
+        {
+            double result;
+            switch (sign)
+            {
+                case "+":
+                    result = Summ(a, b);
+                    break;
+                case "-":
+                    result = Diff(a, b);
+                    break;
+                case "x":
+                    result = Multiplication(a, b);
+                    break;
+                case "/":
+                    result = Division(a, b);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return result;
+        }
+    }
+}
---
+++ WpfCalculator/MainCalc.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using MethodsLibrary;
+
+namespace WpfCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainCalc.xaml
+    /// </summary>
+    public partial class MainCalc : Window
+    {
+        private double? lastValue;
+        private string sign;
+        public MainCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_All_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = "";
+            lastValue = null;
+            sign = null;
+        }
+
+        private void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text += ((Button) e.OriginalSource).Content;
+        }
+
+        private void Operatoin_Click(object sender, RoutedEventArgs e)
+        {
+            var value = Double.Parse(InputTextBox.Text);
+            if (lastValue == null)
+            {
+                lastValue = value;
+            }
+            else
+            {
+                lastValue = MathMethods.Calculate((double)lastValue, value, sign);
+            }
+            OutputTextBox.Text = lastValue + (string)((Button)e.OriginalSource).Content;
+            sign = (string) (((Button) e.OriginalSource).Content);
+            InputTextBox.Text = "";
+        }
+
+        private void Result_Click(object sender, RoutedEventArgs e)
+        {
+            var value = Double.Parse(InputTextBox.Text);
+            if (lastValue == null)
+            {
+                lastValue = value;
+            }
+            else
+            {
+                lastValue = MathMethods.Calculate((double)lastValue, value, sign);
+            }
+            InputTextBox.Text = lastValue.ToString();
+            OutputTextBox.Text = "";
+            lastValue = null;
+        }
+
+    }
+}
